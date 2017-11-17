020e7e4422d5b2e54213701b3eb5e77badb7a76e
lperry34 <leviperry34@gmail.com>
2017-11-17 15:14:27 -0800

    just realized I didn't even make a repo. My bad, I will start commiting now

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'users#index'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  get '/login', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+end
